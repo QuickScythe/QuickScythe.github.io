e2d0eb296c38d6ebea25322a440618844f7407ee
QuickScythe <QuickScythe@gmail.com>
2016-08-04 13:38:21 -0400

    umm

--- rakefile.rb
+++ rakefile.rb
@@ -1,6 +1,9 @@
 
 task :test do
   puts "testing"
+  sh "gem install rubygems-update"
+  sh "sudo update_rubygems"
+  sh "sudo gem update --system"
   sh "gem install jekyll"
   sh "jekyll serve"
 end
